2a0e2741262dc17800bd67d8454d66cd1b05d6de
nurullo <muhitdinovnurullo46@gmail.com>
2024-11-24 17:34:36 +0500

    tes

--- EXAM/settings.py
+++ EXAM/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-o8nnk^0rca^r!xxkmpxw14n&5emdl(1n@sq8k0gn-b@lqkzza9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
